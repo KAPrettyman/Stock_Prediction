b00ed5fc177603e858de93487214dd0eaf6205bb
KAPrettyman <k.a.prettyman@tcu.edu>
2026-03-03 16:24:20 -0600

    Update StreamlitApp_Week_7.py

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -111,19 +111,21 @@ def call_model_api(input_df):
 def display_explanation(input_df, session, aws_bucket):
     explainer_name = MODEL_INFO["explainer"]
     explainer = load_shap_explainer(session, aws_bucket, posixpath.join('explainer', explainer_name),os.path.join(tempfile.gettempdir(), explainer_name))
-    
-    X_test_transformed = preprocessing_pipeline.transform(X_test)
+
+    best_pipeline = load_pipeline(session, aws_bucket,'sklearn-pipeline-deployment')
+    preprocessing_pipeline = Pipeline(steps=best_pipeline.steps[:-2])
+    input_df_transformed = preprocessing_pipeline.transform(input_df)
     feature_names = best_pipeline[1:4].get_feature_names_out()
-    X_test_transformed = pd.DataFrame(X_test_transformed, columns=feature_names)
-    shap_values = explainer(X_test_transformed)
+    input_df_transformed = pd.DataFrame(input_df_transformed, columns=feature_names)
+    shap_values = explainer(input_df_transformed)
     
-    shap_values = explainer(input_df)
+   
     st.subheader("🔍 Decision Transparency (SHAP)")
     fig, ax = plt.subplots(figsize=(10, 4))
-    shap.plots.waterfall(shap_values[0], max_display=10)
+    shap.plots.waterfall(shap_values[0,:,0], max_display=10)
     st.pyplot(fig)
     # top feature   
-    top_feature = shap_values[0].feature_names[0]
+    top_feature = pd.Series(shap_values[0,:,0].values, index=shap_values[0,:,0].feature_names).abs().idxmax()
     st.info(f"**Business Insight:** The most influential factor in this decision was **{top_feature}**.")
 
 # Streamlit UI
@@ -157,6 +159,7 @@ if submitted:
         display_explanation(input_df,session, aws_bucket)
     else:
         st.error(res)
+
 
 
 
